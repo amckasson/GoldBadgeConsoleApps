1ad4769fd617fa7f1866cbf48fa7be8341a114ff
Andrew McKasson <amckasson90@gmail.com>
2021-05-16 13:39:41 -0400

    Third Commit initial tests added

---
+++ KomodoCafe_Tests/MenuItemsRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using KomodoCafe_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KomodoCafe_Tests
+{
+    [TestClass]
+    public class MenuItemsRepositoryTests
+    {
+        [TestMethod]
+        public void AddToList_ShouldGetNotNull()
+        {
+            MenuItems item = new MenuItems();
+            item.MealName = "Burger";
+            MenuItemRepository repositroy = new MenuItemRepository();
+
+            repositroy.AddItemToList(item);
+            MenuItems itemsFromMenu = repositroy.GetItemsByName("Burger");
+
+            Assert.IsNotNull(itemsFromMenu);
+        }
+    }
+}
---
+++ KomodoCafe_Tests/MenuItemsTests.cs
@@ -0,0 +1,23 @@
+﻿using KomodoCafe_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KomodoCafe_Tests
+{
+    [TestClass]
+    public class MenuItemsTests
+    {
+        [TestMethod]
+        public void SetMenuName_ShouldSetCorrectString()
+        {
+            MenuItems item = new MenuItems();
+
+            item.MealName = "Burger";
+
+            string expected = "Burger";
+            string actual = item.MealName;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
