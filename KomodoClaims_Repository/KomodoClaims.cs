6d26a4a22a905e1cae7d2b463381b3dc585cad8e
Andrew McKasson <amckasson90@gmail.com>
2021-05-17 10:33:21 -0400

    ThirdCommit ClaimPOCO added

---
+++ KomodoClaims_Repository/KomodoClaims.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoClaims_Repository
+{
+    public enum ClaimType { Car = 1, Home, Theft}
+    public class KomodoClaims
+    {
+        public int ClaimID { get; set; }
+        public ClaimType TypeOfClaim { get; set; }
+        public string Description { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+
+        public KomodoClaims() { }
+
+        public KomodoClaims(int claimID, ClaimType typeOfClaim, string description, decimal claimAmount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
+        {
+            ClaimID = claimID;
+            TypeOfClaim = typeOfClaim;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+            IsValid = isValid;
+        }
+    }
+}
